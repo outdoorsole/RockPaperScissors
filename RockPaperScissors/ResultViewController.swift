8d8c2855a420828ea08ffc2425bdb5163413f0ee
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-03-12 19:26:14 -0700

    Add back button for ResultVC

---
+++ RockPaperScissors/ResultViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultViewController.swift
+//  RockPaperScissors
+//
+//  Created by Maribel Montejano on 3/12/18.
+//  Copyright © 2018 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
